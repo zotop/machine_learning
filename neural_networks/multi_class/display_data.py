71565e01e3c98f6a84d67d9d0ba73a75d9e7eaed
Joao Guerreiro <jmdguerreiro@gmail.com>
2017-12-15 22:35:17 +0100

    displaying 100 random selected numbers of the training set

---
+++ neural_networks/multi_class/display_data.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.io import loadmat	
+import random
+
+data = loadmat('data.mat')
+
+random_numbers = random.sample(range(0, 5000), 100)
+matrix = None
+index = 0
+
+for i in range(0, 10):
+	row = None
+	for j in range(0, 10):
+		number = (data['X'][random_numbers[index]]).reshape((20, 20)).transpose()
+		if (row == None):
+			row = number
+		else:
+			row = np.concatenate((row, number), axis=1)
+
+		index += 1
+			
+	if (matrix == None):
+		matrix = row
+	else:
+		matrix = np.concatenate((matrix, row), axis=0) 
+
+
+im = plt.imshow(matrix, cmap='gray')
+plt.show()
