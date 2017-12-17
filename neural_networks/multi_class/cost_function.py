64bd0d30e06b697d3da40df45c817b6a7bbc1a5f
Joao Guerreiro <jmdguerreiro@gmail.com>
2017-12-17 22:38:12 +0100

    work in progress

--- neural_networks/multi_class/cost_function.py
+++ neural_networks/multi_class/cost_function.py
@@ -4,24 +4,11 @@ from scipy.io import loadmat
 def sigmoid(z):
 	return float(1)/( 1 + np.exp(-z))
 
-def h_theta(theta, x):
-	return sigmoid(x.dot(theta))
-
 def cost(theta, x, y, lambda_value):
 	m = len(x)
-	regularization_term = ( float(lambda_value)/(2*m) ) * (theta[1:theta.shape[0]]**2).sum()
-	cost_value = 0
-	h_theta_value = h_theta(theta, x)
-	term1 = y.dot(np.log(h_theta_value).transpose())
-	term2 = (1 - y).dot(np.transpose(np.log( 1 - h_theta_value )) )
-	cost_value += (term1 + term2).sum()
-	cost_value *=  (-float(1)/m)	
-	return 	cost_value
- 
-
-data = loadmat('data.mat')
-x = data['X']
-y = data['y']
-
-initial_theta = np.zeros((x.shape[1], 1))
-print cost(initial_theta, x, y, 1)
+	h_theta_value = sigmoid(np.dot(x,theta))
+	term1 = y.transpose().dot(np.log(h_theta_value))
+	term2 = (1 - y).transpose().dot( np.log( 1 - h_theta_value) )
+	cost_value = -(1./m) * (term1 + term2)
+	regularization_term = float(lambda_value)/(2*m) * (theta[1:len(theta)]**2).sum()
+	return 	(cost_value + regularization_term).item(0)
---
+++ neural_networks/multi_class/one_vs_all.py
@@ -0,0 +1,15 @@
+from scipy.io import loadmat
+import scipy.optimize as opt
+import numpy as np
+import cost_function as cf
+
+data = loadmat('data.mat')
+x = data['X']
+y = data['y']
+m, n = x.shape
+x = np.column_stack((np.ones((m,1)), x))
+initial_theta = np.zeros((x.shape[1], 1))
+lambda_value = 0.1
+initial_theta = np.zeros((n + 1, 1))
+
+min_result = opt.minimize(cf.cost, x0=initial_theta, args=(x, (y%10==0).astype(int).transpose()[0], lambda_value), options={'disp': True, 'maxiter':10}, method="BFGS")
