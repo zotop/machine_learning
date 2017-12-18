4cf3dcc0ec6f6908b9302f95075756eb4255f903
Joao Guerreiro <jmdguerreiro@gmail.com>
2017-12-18 13:26:17 +0100

    calculating thetas for each digit

--- neural_networks/multi_class/one_vs_all.py
+++ neural_networks/multi_class/one_vs_all.py
@@ -12,4 +12,12 @@ initial_theta = np.zeros((x.shape[1], 1))
 lambda_value = 0.1
 initial_theta = np.zeros((n + 1, 1))
 
-min_result = opt.minimize(cf.cost, x0=initial_theta, args=(x, (y%10==0).astype(int).transpose()[0], lambda_value), options={'disp': True, 'maxiter':13}, method="Newton-CG", jac=True)
+number_of_digits = 10
+thetas = np.zeros((number_of_digits, n + 1))
+
+for i in range(1, number_of_digits + 1):
+	theta = opt.minimize(cf.cost, x0=initial_theta, args=(x, (y%10==i).astype(int).transpose()[0], lambda_value), options={'disp': False, 'maxiter':13}, method="Newton-CG", jac=True)
+	thetas[i%10] = theta.x
+	print "Done calculating thetas for digit {}".format(i%10)
+
+
